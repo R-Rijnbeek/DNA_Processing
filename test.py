cb2220c59ce4e0e37aa7c0b26584bb654fa319cd
R-Rijnbeek <robert270384@gmail.com>
2021-01-13 18:39:27 +0100

    first commit to push to remote repository

--- test.py
+++ test.py
@@ -1,9 +1,66 @@
 def loadDNAFile(path):
-    return open( path, "r").read().replace("\n", "").replace("\r", "") 
+    return open( path, "r").read().replace("\n", "").replace("\r", "")
 
-dnaString = loadDNAFile("DATA\DNA\DNA_seq_altered.txt")
+def isDNAComplete(dnaSequence):
+    return len(dnaSequence)%3 == 0
 
-print(dnaString)
+def findAUG(dnaSequence):
+    return dnaSequence.find("AUG")
+
+def idSequenceDNA(seq):
+    removeDuplicates =  "".join(set(seq))
+    for char in removeDuplicates:
+        if char not in ["A","T","C","G"]:
+            return False
+    return True
+
+def createRNAbyDNA(dnaSequence):
+    return dnaSequence.replace("T","U")
+
+def createDNAbyRNA(rnaSequence):
+    return rnaSequence.replace("U","T")
+
+def translateARNtoProtein(seq):
+    table = { 
+        'AUA':'I', 'AUC':'I', 'AUU':'I', 'AUG':'M', 
+        'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACU':'T', 
+        'AAC':'N', 'AAU':'N', 'AAA':'K', 'AAG':'K', 
+        'AGC':'S', 'AGU':'S', 'AGA':'R', 'AGG':'R', 
+        'CUA':'L', 'CUC':'L', 'CUG':'L', 'CUU':'L', 
+        'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCU':'P', 
+        'CAC':'H', 'CAU':'H', 'CAA':'Q', 'CAG':'Q', 
+        'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGU':'R', 
+        'GUA':'V', 'GUC':'V', 'GUG':'V', 'GUU':'V', 
+        'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCU':'A', 
+        'GAC':'D', 'GAU':'D', 'GAA':'E', 'GAG':'E', 
+        'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGU':'G', 
+        'UCA':'S', 'UCC':'S', 'UCG':'S', 'UCU':'S', 
+        'UUC':'F', 'UUU':'F', 'UUA':'L', 'UUG':'L', 
+        'UAC':'Y', 'UAU':'Y', 'UAA':'_', 'UAG':'_', 
+        'UGC':'C', 'UGU':'C', 'UGA':'_', 'UGG':'W',
+    } 
+    protein ="" 
+
+    for i in range(0, len(seq), 3): 
+        codon = seq[i:i + 3] 
+        try:
+            protein+= table[codon]
+        except :
+            pass
+    return protein 
+
+
+
+if __name__ == "__main__":
+
+    dnaString = loadDNAFile("DATA\\DNA\\DNA_seq_altered.txt")
+
+    print(dnaString)
+    print(isDNAComplete(dnaString))
+    print(idSequenceDNA(dnaString))
+    rna = createRNAbyDNA(dnaString)
+    print(findAUG(rna))
+    print(translateARNtoProtein(rna[20:935]))
 
 
     
