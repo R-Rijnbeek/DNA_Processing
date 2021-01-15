b7d02635d7a7a783c4566acfe5879201410eebd8
R-Rijnbeek <robert270384@gmail.com>
2021-01-15 11:02:33 +0100

    Finished TestModule

--- TEST/prueba.py
+++ TEST/prueba.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append('.')
 
-from DNA_RNA_Protein_Module import DNA, RNA, Protein,loadFile
+from DNA_RNA_Protein_Module import DNA, RNA, Protein, loadFile, findProteinsequences
 
 def test_1():
     dnaString = loadFile("DATA\\DNA\\DNA_seq_altered.txt")
@@ -9,6 +9,7 @@ def test_1():
     rna = dna.createRNA()
     dna1 = rna.createDNA()
     if (dna.getSequence() == dna1.getSequence()):
+        print("TEST 1: SUCCESS")
         return True
     return False
 
@@ -18,6 +19,7 @@ def test_2():
     dna.writeToFile("DATA\\DNA\\DNA_test_2.txt")
     dnaString_2 = loadFile("DATA\\DNA\\DNA_test_2.txt")
     if (dnaString_1 == dnaString_2):
+        print("TEST 2: SUCCESS")
         return True
     return False
 
@@ -27,8 +29,31 @@ def test_3():
     rna = dna.createRNA()
     rna.writeToFile("DATA\\RNA\\RNA_test_3.txt")
     rnaString = loadFile("DATA\\RNA\\RNA_test_3.txt")
-    if ( (rna.getSequence() == rnaString) and (dna.getSequence() == dnaString) ) :
+    if ( (rna.getSequence() == rnaString) and (dna.getSequence() == dnaString) ):
+        print("TEST 3: SUCCESS")
         return True
+    return False
+
+def test_4():
+    dnaString = loadFile("DATA\\DNA\\DNA_test_2.txt")
+    dna = DNA(dnaString)
+    rnaString = loadFile("DATA\\RNA\\RNA_test_3.txt")
+    rna = RNA(rnaString)
+    rna_beginPositions = rna.findBeginSequence()
+    rna_endPositions = rna.findEndSequence()
+    dna_beginPositions = dna.findBeginSequence()
+    dna_endPositions = dna.findEndSequence()
+    if ((rna_beginPositions == dna_beginPositions) and (rna_endPositions == dna_endPositions)):
+        valid_sequences = findProteinsequences(rna_beginPositions,rna_endPositions)
+        firstInterval = valid_sequences[0]
+        rna_subsection = rna.getSubSection(firstInterval)
+        protein = rna_subsection.createProtein()
+        protein.writeToFile("DATA\\PROTEIN\\Protein_test4.txt")
+        builded_protein = loadFile("DATA\\PROTEIN\\Protein_test4.txt")
+        valid_protein = loadFile("DATA\\PROTEIN\\amino_acid_sequence_original.txt")
+        if ((protein.getSequence() == valid_protein) and (builded_protein == valid_protein)):
+            print("TEST 4: SUCCESS")
+            return True
     return False
 
 def testProcess():
@@ -38,7 +63,9 @@ def testProcess():
         if (test_1()):
             if (test_2()):
                 if (test_3()):
-                    return True
+                    if test_4():
+                        print("TESTS SUCCESSFULL PASSED")
+                        return True
         return False
     except Exception as exc:
         print(f'ERROR: {exc}')
