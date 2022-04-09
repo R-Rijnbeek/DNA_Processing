5ec1aba3033dbe86920c52663a95516a481431be
R-Rijnbeek <robert270384@gmail.com>
2022-04-09 19:21:04 +0200

    Make file path os independent

--- TESTS/__init__.py
+++ TESTS/__init__.py
@@ -7,10 +7,12 @@ test module for dna_processing: Test dodule to test the DNA_ProcessPackage
 
 from dna_processing import DNA, RNA, Protein, loadFile, findProteinsequences
 
+from os.path import join
+
 # ======== TEST FUNCTIONS =======
 
 def test_1():
-    dnaString = loadFile("DATA\\DNA\\DNA_seq_altered.txt")
+    dnaString = loadFile(join("DATA", "DNA", "DNA_seq_altered.txt"))
     dna = DNA(dnaString)
     rna = dna.createRNA()
     dna1 = rna.createDNA()
@@ -20,30 +22,30 @@ def test_1():
     return False
 
 def test_2():
-    dnaString_1 = loadFile("DATA\\DNA\\DNA_seq_altered.txt")
+    dnaString_1 = loadFile(join("DATA", "DNA", "DNA_seq_altered.txt"))
     dna = DNA(dnaString_1)
-    dna.writeToFile("DATA\\DNA\\DNA_test_2.txt")
-    dnaString_2 = loadFile("DATA\\DNA\\DNA_test_2.txt")
+    dna.writeToFile(join("DATA", "DNA", "DNA_test_2.txt"))
+    dnaString_2 = loadFile(join("DATA", "DNA", "DNA_test_2.txt"))
     if (dnaString_1 == dnaString_2):
         print("TEST 2: SUCCESS")
         return True
     return False
 
 def test_3():
-    dnaString = loadFile("DATA\\DNA\\DNA_seq_altered.txt")
+    dnaString = loadFile(join("DATA", "DNA", "DNA_seq_altered.txt"))
     dna = DNA(dnaString)
     rna = dna.createRNA()
-    rna.writeToFile("DATA\\RNA\\RNA_test_3.txt")
-    rnaString = loadFile("DATA\\RNA\\RNA_test_3.txt")
+    rna.writeToFile(join("DATA", "RNA", "RNA_test_3.txt"))
+    rnaString = loadFile(join("DATA", "RNA", "RNA_test_3.txt"))
     if ( (rna.getSequence() == rnaString) and (dna.getSequence() == dnaString) ):
         print("TEST 3: SUCCESS")
         return True
     return False
 
 def test_4():
-    dnaString = loadFile("DATA\\DNA\\DNA_test_2.txt")
+    dnaString = loadFile(join("DATA", "DNA", "DNA_test_2.txt"))
     dna = DNA(dnaString)
-    rnaString = loadFile("DATA\\RNA\\RNA_test_3.txt")
+    rnaString = loadFile(join("DATA", "RNA", "RNA_test_3.txt"))
     rna = RNA(rnaString)
     rna_beginPositions = rna.findBeginSequence()
     rna_endPositions = rna.findEndSequence()
@@ -54,9 +56,9 @@ def test_4():
         firstInterval = valid_sequences[0]
         rna_subsection = rna.getSubSection(firstInterval)
         protein = rna_subsection.createProtein()
-        protein.writeToFile("DATA\\PROTEIN\\Protein_test4.txt")
-        builded_protein = loadFile("DATA\\PROTEIN\\Protein_test4.txt")
-        valid_protein = loadFile("DATA\\PROTEIN\\amino_acid_sequence_original.txt")
+        protein.writeToFile(join("DATA", "PROTEIN", "Protein_test4.txt"))
+        builded_protein = loadFile(join("DATA", "PROTEIN", "Protein_test4.txt"))
+        valid_protein = loadFile(join("DATA", "PROTEIN", "amino_acid_sequence_original.txt"))
         if ((protein.getSequence() == valid_protein) and (builded_protein == valid_protein)):
             print("TEST 4: SUCCESS")
             return True
